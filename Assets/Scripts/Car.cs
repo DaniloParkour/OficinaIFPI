4e8fe8c0e36ccac213301405aaeaf564f61778e2
daniloparkour <danilomlcarvalho@gmail.com>
2021-04-07 23:32:34 -0300

    Finish first day

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+  public GameObject Bullet;
+
+    // Start is called before the first frame update
+  void Start()
+  {
+        
+  }
+
+    // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.W))
+      transform.Translate(transform.forward * Time.deltaTime * 10);
+    else if (Input.GetKey(KeyCode.S))
+      transform.Translate(transform.forward * -1 * Time.deltaTime * 10);
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      GameObject b = Instantiate(Bullet);
+      b.transform.position = transform.position;
+    }
+
+  }
+
+}
